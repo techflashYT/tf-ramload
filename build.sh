776a682c0a715f74d8c53cd5906409b6f310f60e
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-12-23 13:56:30 -0700

    A good start, busybox is a thing

--- build.sh
+++ build.sh
@@ -9,10 +9,55 @@ MODULES=(
     drm_ttm_helper
 )
 
+build="$PWD/_buildTmp"
+busyboxDir="$PWD/busybox-1.36.1"
+busyboxFile="https://busybox.net/downloads/busybox-1.36.1.tar.bz2"
+busyboxConfig="busyboxConfig"
+
 FILES=(
     "$PWD/ramload.sh:/init"
+    "$busyboxDir/busybox:/bin/busybox"
 )
 
-rm -rf _buildTmp
-mkdir _buildTmp
+rm -rf "$build"
+mkdir "$build"
+
+
+# check if we have busybox
+if ! [ -d "$busyboxDir" ]; then
+    wget "$busyboxFile"
+    exitCode=$?
+    if [ $exitCode != 0 ]; then
+        echo "Busybox download failed with exit code $exitCode" >&2
+        exit 1
+    fi
+    tar xf "$busyboxFile"
+    cp "$busyboxConfig" "$busyboxDir/.config"
+fi
+
+# build busybox
+pushd "$busyboxDir" > /dev/null
+make
+popd > /dev/null
+
+
+
+# make base initrd structure
+cd "$build"
+mkdir -p usr/bin usr/lib usr/share etc
+ln -s usr/bin bin
+ln -s usr/lib lib
+ln -s usr/lib usr/lib64
+ln -s usr/lib64 lib64
+cd ..
+
+# get the files and where they need to go
+for file in ${FILES[@]}; do
+    hostPath=$(echo $file | cut -f1 -d:)
+    initrdPath=$(echo $file | cut -f2 -d:)
+
+    echo "copying $hostPath to $build$initrdPath"
+    cp "$hostPath" "$build$initrdPath"
+done
+
 
