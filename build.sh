7957826f8517578f85b57c3e225ad49f352efe12
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-12-23 16:22:52 -0700

    Stuff can load, but init fails

--- build.sh
+++ build.sh
@@ -13,8 +13,10 @@ build="$PWD/_buildTmp"
 busyboxDir="$PWD/busybox"
 busyboxFile="https://busybox.net/downloads/busybox-1.36.1.tar.bz2"
 busyboxConfig="busyboxConfig"
-linuxKernelFile="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.8.tar.xz"
-linuxKernelDir="linux"
+linuxFile="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.8.tar.xz"
+linuxDir="linux"
+linuxConfig="linuxConfig"
+kernelPath="$linuxDir/arch/x86/boot/bzImage"
 
 FILES=(
     "$PWD/ramload.sh:/init"
@@ -49,39 +51,53 @@ function dload() {
 
 
 rm -rf "$build"
-mkdir "$build"
+mkdir "$build/initramfs" -p
 
 
 # check if we have busybox
 dload "$busyboxDir" "$busyboxFile" "$busyboxConfig" "Busybox"
 
-
+# check if we have the Linux kernel
+dload "$linuxDir" "$linuxFile" "$linuxConfig" "Linux kernel"
 
 
 
 # build busybox
 pushd "$busyboxDir" > /dev/null
-make
+make -j$(nproc)
 popd > /dev/null
+
+
+# build the Linux kernel
+pushd "$linuxDir" > /dev/null
+make -j$(nproc)
+popd > /dev/null
+cp "$kernelPath" "$build/"
+
 
 
 
 # make base initrd structure
-cd "$build"
+pushd "$build/initramfs" > /dev/null
 mkdir -p usr/bin usr/lib usr/share etc
 ln -s usr/bin bin
 ln -s usr/lib lib
 ln -s usr/lib usr/lib64
 ln -s usr/lib64 lib64
-cd ..
+ln -s busybox bin/sh
+popd > /dev/null
 
 # get the files and where they need to go
 for file in ${FILES[@]}; do
     hostPath=$(echo $file | cut -f1 -d:)
     initrdPath=$(echo $file | cut -f2 -d:)
 
-    echo "copying $hostPath to $build$initrdPath"
-    cp "$hostPath" "$build$initrdPath"
+    echo "copying $hostPath to $build/initramfs$initrdPath"
+    cp "$hostPath" "$build/initramfs$initrdPath"
 done
 
+pushd $build/initramfs > /dev/null
+# find . | cpio -o --format='newc' | lz4 -3 - ../initramfs.img
+find . | cpio -o --format='newc' > ../initramfs.img
 
+popd > /dev/null
--- ramload.sh
+++ ramload.sh
@@ -1,1 +1,2 @@
 #/bin/sh
+/bin/busybox --install -s
