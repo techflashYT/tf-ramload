cd1bbf973ae50750813feb636aec2998d6c329f5
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-12-23 22:10:58 -0700

    Greatly expand the scope of the disk checker

--- ramload.sh
+++ ramload.sh
@@ -9,13 +9,37 @@ mount -t sysfs sysfs /sys
 mount -t devtmpfs devtmpfs /dev
 
 for dev in $(echo /sys/class/block/* | tr ' ' '\n' | grep -vE 'loop[0-9]'); do
+    # get disk size
     size=$(cat "$dev/size")
+
+    # get some env vars about the disk
     eval "$(blkid -o export "/dev/$(basename "$dev")")"
-    echo "blkdev: $(basename "$dev"); size=$(($size * 512)) bytes; type=$TYPE"
-    if [ "$PTTYPE" = "dos" ] || [ "$PTTYPE" = "gpt" ]; then
-        echo "$(basename "$dev") is a full disk w/ partition table, not a filesystem."
+
+    # print the info
+    echo "blkdev: $(basename "$dev"); size=$(($size * 512)) bytes; type=$PTTYPE"
+
+    # print info about the type, particularly if it is odd
+    case "$PTTYPE" in
+        "dos"|"gpt")
+            str="a full disk w/ partition table, not a filesystem.  Skipping." ;;
+        "")
+            str="an empty/unknown filesystem.  Skipping." ;;
+        "ext"*|"xfs"|"btrfs")
+            str="a normal on-disk filesystem.  Marked for checking."
+            checkme=true ;;
+        "vfat"|*"msdos")
+            str="\
+some form of FAT.  EFI System Partition?  \
+Either way, neither suitable for a distro nor shared info partition.  Skipping." ;;
+        *)
+            str="an unknown filesystem type \"$PTTYPE\"!  Please report this error!  Skipping." ;;
+    esac
+    unset PTUUID PARTUUID BLOCK_SIZE DEVNAME PART_SIZE TYPE UUID PARTLABEL PTTYPE
+
+    echo "$(basename "$dev") is $str"
+    if [ "$checkme" = "true" ]; then
+        echo "checking this partition"
     fi
-    unset PTUUID PARTUUID BLOCK_SIZE DEVNAME PART_SIZE TYPE UUID PARTLABEL
 
 done
 ash
