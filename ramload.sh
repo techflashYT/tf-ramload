7d044e54eb08628bd5bdf20ba996e54b8d1cf233
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-12-23 17:31:30 -0700

    start experimenting with stuff

--- ramload.sh
+++ ramload.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 /bin/busybox --install -s
-while true; do
-    # nothing
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+
+# mount stuff
+mkdir proc sys
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+
+for dev in $(echo /sys/class/block/* | tr ' ' '\n' | grep -vE 'loop[0-9]'); do
+    echo "blkdev: $(basename "$dev"); size=$(cat "$dev/size")"
 done
+ash
