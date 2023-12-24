e5d07e3a1a7aab41ca7a14d267ec69d5541a88b6
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-12-23 21:23:43 -0700

    start trying to find a partition using blkid

--- ramload.sh
+++ ramload.sh
@@ -6,8 +6,16 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 mkdir proc sys
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev
 
 for dev in $(echo /sys/class/block/* | tr ' ' '\n' | grep -vE 'loop[0-9]'); do
-    echo "blkdev: $(basename "$dev"); size=$(cat "$dev/size")"
+    size=$(cat "$dev/size")
+    eval "$(blkid -o export "/dev/$(basename "$dev")")"
+    echo "blkdev: $(basename "$dev"); size=$(($size * 512)) bytes; type=$TYPE"
+    if [ "$PTTYPE" = "dos" ] || [ "$PTTYPE" = "gpt" ]; then
+        echo "$(basename "$dev") is a full disk w/ partition table, not a filesystem."
+    fi
+    unset PTUUID PARTUUID BLOCK_SIZE DEVNAME PART_SIZE TYPE UUID PARTLABEL
+
 done
 ash
